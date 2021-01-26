ea2b23c7d7ceb418206c502e0023989c528943c4
“breogann” <fernandocostapradillo@gmail.com>
2021-01-26 14:06:26 +0100

    monica

--- installations.sh
+++ installations.sh
@@ -1,15 +1,19 @@
 #Installing Homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
+say -v Monica "bru instalado"
 
 #Installing zsh + ohmyzsh + iterm2
 brew install --cask iterm2
 brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" #install zsh
 chsh -s /usr/local/bin/zsh #changes the shell to zsh. It should be already set by the previous line, but just to be sure.
+say -v Monica "terminal instalada"
+
 
 #Installing git
 brew install git
+say -v Monica "git instalado"
 
 #Linking Git with Github
 git config --global user.name “YOUR_GITHUB_USERNAME”;
@@ -17,24 +21,28 @@ git config --global user.email YOUR_EMAIL@DOMAIN.COM
 
 #Python
 brew install python3
+say -v Monica "paizon instalado"
+
 
 #Pip3 & jupyter
-say -v Monica "¿Cuál es la contraseña del equipo?"
 pip install --upgrade pip
+say -v Monica "pip instalado"
 
-say -v Monica "¿De nuevo, ¿la contraseña?"
+
+say -v Monica "¿Cuál es la contraseña?"
 sudo python3 -m pip install jupyter
-
-pip install --upgrade pip
-
+say -v Monica "yúpiter instalado"
 
 
 #Miniconda
 brew install --cask miniconda
+say -v Monica "miniconda instalado"
 
-#MySQL, Heroku (comment the following line if unnecessary):
-brew install mysql
+
+#Heroku
 brew tap heroku/brew && brew install heroku
+say -v Monica "Heroku instalado"
+
 
 #MongoDB
 brew tap mongodb/brew
@@ -42,6 +50,8 @@ brew install mongodb-community@4.4 #Install
 brew services start mongodb-community@4.4 #Start
 brew install mongodb/brew/mongodb-database-tools #For future mongodump and mongorestore
 #mongorestore dump/
+say -v Monica "Mongo instalado"
+
 
 
 #Checking everything's up to date
@@ -61,6 +71,9 @@ brew install --cask homebrew/cask-versions/sequel-pro-nightly
 brew install --cask mysqlworkbench
 
 #MySQL
+brew install mysql
+say -v Monica "sícuel instalado"
+
 mysql.server start
 sudo mysql -u root
 USE mysql;
